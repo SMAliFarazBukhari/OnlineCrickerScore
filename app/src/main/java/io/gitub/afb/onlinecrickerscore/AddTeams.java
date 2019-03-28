08b479ce7b0a12ffa18c40ef0fae38ce1fbd2d82
SMAliFarazBukhari <46835829+SMAliFarazBukhari@users.noreply.github.com>
2019-03-28 13:53:35 +0500

    Completed Add Team Activity, with browsing image from gallery and showing on image view

--- app/src/main/java/io/gitub/afb/onlinecrickerscore/AddTeams.java
+++ app/src/main/java/io/gitub/afb/onlinecrickerscore/AddTeams.java
@@ -2,23 +2,41 @@ package io.gitub.afb.onlinecrickerscore;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class AddTeams extends Activity {
 
-    ImageView menuImage;
+    private ImageView menuImage,SelectedImage;
+    private String selectedImagePath;
+    private Button browseBtn;
+    private static final int SELECT_PICTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_teams);
         getActionBar().hide();
+        SelectedImage = findViewById(R.id.imgflag);
 
-
+        browseBtn= findViewById(R.id.browseBtn);
+        browseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+            }
+        });
 
         //Dotted Menu Image Stuff
         menuImage = findViewById(R.id.menu3dotimage);
@@ -31,6 +49,26 @@ public class AddTeams extends Activity {
 
 
 
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                SelectedImage.setImageURI(selectedImageUri);
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 
 
